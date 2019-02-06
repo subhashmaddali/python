cd24823825dad1a42d1b62f2bd1e78a6c077c1ef
Subhash <MSubhash@ad.harman.com>
2019-02-06 17:42:53 +0530

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,16 @@
+from __future__ import division
+
+def lambda_handler(event, context):
+   number1 = event['Number1']
+   number2 = event['Number2']
+   sum = number1 + number2
+   product = number1 * number2
+   difference = abs(number1 - number2)
+   quotient = number1 / number2
+   return {
+       "Number1": number1,
+       "Number2": number2,
+       "Sum": sum,
+       "Product": product,
+       "Difference": difference
+   }
